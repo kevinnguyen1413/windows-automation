a4273a04299c1651c5e0692b721893ba2b5e430e
Kevin Nguyen <kevinnguyen1413@gmail.com>
2023-08-05 18:54:09 +0700

    Add function to check number of folders and delete/create based on specified parameters

--- backup_methodv1.ps1
+++ backup_methodv1.ps1
@@ -3,3 +3,23 @@
 function main {
     checkanddeletebackup
 }
+
+function checkanddeletebackup {
+    if ($count -le 1) {
+        Write-Host $count 'backup found. Creating new backup'
+        createandcopy
+    } elseif ($count -ge 2) {
+        Write-Host 'There are' $count 'backups. Checking for sufficient free space to create backup number' ($count+1)
+        checkenoughspace
+        if((checkenoughspace) -eq $true) {
+            createandcopy
+        } elseif ((checkenoughspace) -ne $true)  {
+            deleteoldestbackup
+            createandcopy
+            exit
+        }
+    } else {
+        Write-Host 'Please check the code for errors'
+        exit
+    }
+}
