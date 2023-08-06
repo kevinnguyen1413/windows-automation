485d48f53c1d32d8bf85244f4337e8e1c3bc2f26
Kevin Nguyen <43601216+kevinnguyen1413@users.noreply.github.com>
2023-08-07 01:41:30 +0700

    Write new main function and add appropriate functions to execute main

--- backup_methodv1.ps1
+++ backup_methodv1.ps1
@@ -1,25 +1,61 @@
 
 
 function main {
-    checkanddeletebackup
-}
-
-function checkanddeletebackup {
-    if ($count -le 1) {
-        Write-Host $count 'backup found. Creating new backup'
-        createandcopy
-    } elseif ($count -ge 2) {
-        Write-Host 'There are' $count 'backups. Checking for sufficient free space to create backup number' ($count+1)
+    checkenoughspace
+    checkhowmanybackup
+    if ((checkenoughspace) -eq $false -And (checkhowmanybackup -eq $true)) {
+        deleteoldestbackup
         checkenoughspace
-        if((checkenoughspace) -eq $true -And (checkhowmanybackup) -eq $true) {
+        if ((deleteoldestbackup) -eq $true -And (checkenoughspace -eq $true)) {
             createandcopy
-        } elseif ((checkenoughspace) -ne $true)  {
-            deleteoldestbackup
-            createandcopy
+        } else {
+            Write-Output 'The script has encountered error(s). Please check code.'
             exit
         }
+    } elseif ((checkenoughspace) -eq $true -And (checkhowmanybackup -eq $true)) {
+        createandcopy
     } else {
-        Write-Host 'Please check the code for errors'
+        Write-Output 'The script has encountered error(s). Please check code.'
         exit
     }
+}
+
+function checkenoughspace {
+    if($DestFreeSpace*0.9 -gt $SourceUsedSpace) {
+        Write-Output "Enough space"
+        return $true
+    } else {
+        Write-Output "Not enough space!"
+        return $false
+    }
+}
+
+function checkbackups {
+    if ($count -le 1) {
+        Write-Host $count 'backup found.'
+        return $true
+    } elseif ($count -ge 2) {
+        Write-Host 'There are' $count 'backups.'
+        return $true
+    } else {
+        Write-Output 'The script has encountered error(s). Please check code.'
+        exit
+    }
+}
+
+function deleteoldestbackup {
+    Write-Output 'There are' $count 'backups but not enough drive capacity. Deletion will begin in 5 seconds. Press Ctrl+C if you want to stop now.'
+    Start-Sleep -Seconds 5
+    $allFolders | Sort-Object CreationTime -Descending | Select-Object -Skip 1 | Remove-Item -Recurse -Force
+    Write-Output 'Deletion completed.'
+    return $true
+}
+
+function createandcopy {
+    Write-Output 'Creating new backup.'
+    $folderName = (Get-Date).tostring("yyyy-MM-dd-hh-mm-ss")
+    New-Item -ItemType Directory -Path $DestDrive -Name $folderName
+    $newdir = $DestDrive + $folderName
+    robocopy $SourceDrive "$newdir" /E /A-:SHA /XA:H /XD "$newdir"
+    Write-Output 'Backup created at'$newdir
 }
