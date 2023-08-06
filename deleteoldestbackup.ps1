258442bad6d16f54cd6cc2926ae2199bbb0f1644
Kevin Nguyen <43601216+kevinnguyen1413@users.noreply.github.com>
2023-08-07 01:07:06 +0700

    Add return $true after code execution of folder delete

--- deleteoldestbackup.ps1
+++ deleteoldestbackup.ps1
@@ -2,6 +2,7 @@ function deleteoldestbackup {
     if ((checkenoughspace) -eq $false -And (checkhowmanybackup -eq $true)) {
         $allFolders | Sort-Object CreationTime -Descending | Select-Object -Skip 1 | Remove-Item -Recurse -Force
         Write-Output 'Deletion completed. Creating new backup'
+        return $true
     } else {
         Write-Output 'The script has encountered error(s). Please check code.'
         exit
