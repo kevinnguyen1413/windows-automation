f34cdd5796d8b67e72e11369db727bc4708b38c2
Kevin Nguyen <43601216+kevinnguyen1413@users.noreply.github.com>
2023-08-07 01:40:20 +0700

    Remove if/else statement

--- deleteoldestbackup.ps1
+++ deleteoldestbackup.ps1
@@ -1,12 +1,7 @@
 function deleteoldestbackup {
-    if ((checkenoughspace) -eq $false -And (checkhowmanybackup -eq $true)) {
         Write-Output 'There are' $count 'backups but not enough drive capacity. Deletion will begin in 5 seconds. Press Ctrl+C if you want to stop now.'
         Start-Sleep -Seconds 5
         $allFolders | Sort-Object CreationTime -Descending | Select-Object -Skip 1 | Remove-Item -Recurse -Force
         Write-Output 'Deletion completed.'
         return $true
-    } else {
-        Write-Output 'The script has encountered error(s). Please check code.'
-        exit
-    }
 }
