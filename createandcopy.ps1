e7837b5232453c9ebd7ae7c0d9302baf91f016b4
Kevin Nguyen <43601216+kevinnguyen1413@users.noreply.github.com>
2023-08-07 01:13:54 +0700

    Add if/else statement to execute create folder and copy files

--- createandcopy.ps1
+++ createandcopy.ps1
@@ -1,7 +1,18 @@
 function createandcopy {
-    $folderName = (Get-Date).tostring("yyyy-MM-dd-hh-mm-ss")
-    New-Item -ItemType Directory -Path $DestDrive -Name $folderName
-    $newdir = $DestDrive + $folderName
-    robocopy $SourceDrive "$newdir" /E /A-:SHA /XA:H /XD "$newdir"
-    Write-Output 'Backup created at'$newdir
+    if ((checkenoughspace) -eq $true -And (checkhowmanybackup -eq $true)) {
+        $folderName = (Get-Date).tostring("yyyy-MM-dd-hh-mm-ss")
+        New-Item -ItemType Directory -Path $DestDrive -Name $folderName
+        $newdir = $DestDrive + $folderName
+        robocopy $SourceDrive "$newdir" /E /A-:SHA /XA:H /XD "$newdir"
+        Write-Output 'Backup created at'$newdir
+    } elseif ((deleteoldestbackup) -eq $true) {
+        $folderName = (Get-Date).tostring("yyyy-MM-dd-hh-mm-ss")
+        New-Item -ItemType Directory -Path $DestDrive -Name $folderName
+        $newdir = $DestDrive + $folderName
+        robocopy $SourceDrive "$newdir" /E /A-:SHA /XA:H /XD "$newdir"
+        Write-Output 'Backup created at'$newdir
+    } else {
+        Write-Output 'The script has encountered error(s). Please check code.'
+        exit 
+    }
 }
