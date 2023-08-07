a7ce8986d3898ef8ffb6d48ad30c31d716565e54
Kevin Nguyen <kevinnguyen1413@gmail.com>
2023-08-07 11:43:47 +0700

    Add log to track backup being kept and backup being deleted

--- deleteoldestbackup.ps1
+++ deleteoldestbackup.ps1
@@ -4,9 +4,14 @@ function deleteoldestbackup {
             return $false
         }
     
-        Write-Host 'There are' $count 'backups but not enough drive capacity. Deletion will begin in 5 seconds.'
-        Start-Sleep -Seconds 5    
+        Write-Host 'There are' $count 'backups but not enough drive capacity.'
+        Write-Host 'Keeping latest backup:'
+        $allFolders | Sort-Object CreationTime -Descending | Select-Object -First 1 | Write-Host
+        Write-Host 'Deletion targets are:' 
+        $allFolders | Sort-Object CreationTime -Descending | Select-Object -Skip 1 | Write-Host
+        Write-Host 'Deletion will begin in 5 seconds.'
+        Start-Sleep -Seconds 10
         $allFolders | Sort-Object CreationTime -Descending | Select-Object -Skip 1 | Remove-Item -Recurse -Force
         Write-Host 'Deletion completed.'
         return $true
-    }
+}
