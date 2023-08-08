1109008d9ad41f68c046fbb0d58ef8fcb2b845b5
Kevin Nguyen <kevinnguyen1413@gmail.com>
2023-08-08 23:17:50 +0700

    Change $folderName creation to yyyy-MM-dd only

--- backup_methodv1.ps1
+++ backup_methodv1.ps1
@@ -76,7 +76,7 @@ function deleteoldestbackup {
 
 function createandcopy {
     Write-Host 'Creating new backup.'
-    $folderName = (Get-Date).ToString("yyyy-MM-dd-HH-mm-ss")
+    $folderName = (Get-Date).ToString("yyyy-MM-dd")
     $newdir = Join-Path $DestDrive $folderName
     New-Item -ItemType Directory -Path $newdir
     robocopy "$SourceDrive" "$newdir" /E /A-:SHA /XA:H /XD "$newdir"
